33aa5ebb8e3b21ef6460ab2fe61eee653f771861
Ömer ARMAĞAN <omer_armagan_35@hotmail.com>
2025-02-13 06:08:47 +0300

    BTK Akademi 2025 İzmir Konakta Temel C# Kursumuzda Mustafa KURAL Hocamız ile Yaptığımız Uygulamalar.

--- U1_Merhaba_Dunya/Program.cs
+++ U1_Merhaba_Dunya/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("BTK Akademi! Izmir 2025");
-Console.WriteLine("Mustafa KURAL Hocamiz ile Temel C# Kursumuz");
+Console.WriteLine("Mustafa KURAL Hocamiz ile Temel C# Kursumuzda Uygulama Yapacağiz");
 Console.WriteLine(2025);//tam sayıyı temsil eder
 Console.WriteLine(4.5);//ondalıklı sayıyı temsil eder
 Console.WriteLine(true);//doğru mantıksalı temsil eder
